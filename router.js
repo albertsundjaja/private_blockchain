bff2b38835e34e60f9a9130a553f47caa2cf12b3
albert <grandia3@hotmail.com>
2019-01-22 23:01:30 +0700

    fix route and port

--- router.js
+++ router.js
@@ -7,12 +7,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.get('/block/:uid', handler.HandlerBlock_GET);
 
 app.post('/block', handler.HandlerBlock_POST);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
 
 
